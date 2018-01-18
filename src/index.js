269a5cb4c1f53bfbbe5f5b2bad87dab41216ba41
Alejandro Salazar <alejsala@cisco.com>
2018-01-18 15:11:47 -0800

    Removed boilder plate code

--- src/index.js
+++ src/index.js
@@ -1,67 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
 
-class Square extends React.Component {
+class App extends Component {
 	render() {
-		return (
-			<button className="square">
-				{/* TODO */}
-			</button>
-		);
-	}
-}
-
-class Board extends React.Component {
-	renderSquare(i) {
-		return <Square />;
-	}
-
-	render() {
-		const status = 'Next player: X';
-
 		return (
 			<div>
-				<div className="status">{status}</div>
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
+				<h1>Site under contruction</h1>
 			</div>
-		);
-	}
-}
-
-class Game extends React.Component {
-	render() {
-		return (
-			<div className="game">
-				<div className="game-board">
-					<Board />
-				</div>
-				<div className="game-info">
-					<div>{/* status */}</div>
-					<ol>{/* TODO */}</ol>
-				</div>
-			</div>
-		);
+		)
 	}
 }
 
 // ========================================
 
 ReactDOM.render(
-	<Game />,
+	<App />,
 	document.getElementById('root')
-);
+)
