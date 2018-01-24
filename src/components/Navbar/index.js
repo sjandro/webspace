2c37ffb25e8eb702dc7cb4b3edb274e6d1b2b775
Alejandro Salazar <alejsala@cisco.com>
2018-01-23 20:54:05 -0800

    icons added

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './styles.css'
+import LinkedinIcon from '../../assets/img/linkedin.svg'
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="nav sticky">
+        <div className="nav-bttn-container">
+          <span className="nav-bttn">Home</span>
+          <span className="nav-bttn">About</span>
+          <span className="nav-bttn">Contact</span>
+
+          {/* <span className="nav-bttn-right">
+            <a href="https://www.linkedin.com/in/salazar6/" target="_blank">
+             <img className="icon" src={require('../../assets/img/linkedin.png')} alt="not working..."/>
+            </a>
+          </span>
+          <span className="nav-bttn-right">Test 5</span> */}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './styles.css'
 
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+
 class App extends Component {
 	render() {
 		return (
 			<div className="content">
-				<div>Hello,</div>
-				<div>If you see this I want you to know you are incredible!</div>
+				<Navbar />
+				<Main />
 			</div>
 		)
 	}
