9397f315eac767dff229037ef9d0e8fdb32b30d9
Alejandro Salazar <alejsala@cisco.com>
2018-05-27 15:12:03 -0700

    save the date

--- src/components/SpecialPurpose/index.js
+++ src/components/SpecialPurpose/index.js
@@ -1,112 +1,26 @@
 import React, { Component, Fragment } from 'react'
 import './styles.css'
+import mainImage from '../../assets/img/saveTheDate.jpg'
+import shrub from '../../assets/img/shrub.png'
+import shrub1 from '../../assets/img/shrub1.png'
+import plants from '../../assets/img/plants_web.png'
 
 class SpecialPurpose extends Component {
-  buttonOnClick = () => {
-    window.alert(
-      'Hello my love. Just wanted to tell you I love you and also safe the date! Maybe we can make a web based save the date that we can print into actual cards! Here is a dummy calendar 😊'
-    )
-  }
   render() {
     return (
-      <Fragment>
-        {/* <div>Save The Date!</div> */}
-        <div className="container">
-          <div className="calendar">
-            <header>
-              <h2>
-                June <span className="accent-h2">2019</span>
-              </h2>
-
-              {/* <a className="btn-prev fontawesome-angle-left" href="#" /> */}
-              {/* <a className="btn-next fontawesome-angle-right" href="#" /> */}
-            </header>
-
-            <table>
-              <thead>
-                <tr>
-                  <td>Su</td>
-                  <td>Mo</td>
-                  <td>Tu</td>
-                  <td>We</td>
-                  <td>Th</td>
-                  <td>Fr</td>
-                  <td>Sa</td>
-                </tr>
-              </thead>
-
-              <tbody>
-                <tr>
-                  <td className="prev-month">26</td>
-                  <td className="prev-month">27</td>
-                  <td className="prev-month">28</td>
-                  <td className="prev-month">29</td>
-                  <td className="prev-month">30</td>
-                  <td className="prev-month">31</td>
-                  <td>1</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>3</td>
-                  <td>4</td>
-                  <td>5</td>
-                  <td>6</td>
-                  <td>7</td>
-                  <td>8</td>
-                </tr>
-                <tr>
-                  <td>9</td>
-                  <td>10</td>
-                  <td>11</td>
-                  <td>12</td>
-                  <td>13</td>
-                  <td>14</td>
-                  <td>15</td>
-                </tr>
-                <tr>
-                  <td>16</td>
-                  <td>17</td>
-                  <td>18</td>
-                  <td>19</td>
-                  <td>20</td>
-                  <td>21</td>
-                  <td className="current-day">22</td>
-                  {/* <td className="current-day event">22</td> */}
-                </tr>
-
-                <tr>
-                  <td>23</td>
-                  <td>24</td>
-                  <td>25</td>
-                  <td>26</td>
-                  <td>27</td>
-                  <td>28</td>
-                  <td>29</td>
-                </tr>
-                <tr>
-                  <td>30</td>
-                  <td className="next-month">1</td>
-                  <td className="next-month">2</td>
-                  <td className="next-month">3</td>
-                  <td className="next-month">4</td>
-                  <td className="next-month">5</td>
-                  <td className="next-month">6</td>
-                </tr>
-              </tbody>
-            </table>
+      <div className="card">
+        <img className="cover" src={mainImage} alt="Janelle and I" />
+        <img className="shrub" src={shrub} alt="shrub" />
+        <img className="shrub1" src={shrub1} alt="shrub" />
+        <div className="text">
+          <div className="title card-text">SAVE THE DATE</div>
+          <div className="subtitle card-text">
+            JANELLE <span>&</span> ALEJANDRO
           </div>
-          {/* <!-- end calendar --> */}
-          <div className="button-container">
-            <span
-              className="button button-primary"
-              onClick={this.buttonOnClick}
-            >
-              Click Nelly!
-            </span>
-          </div>
+          <div className="cursive card-text">June 22, 2019, Tampa</div>
+          <div className="cursive card-text">Invitation to Follow</div>
         </div>
-        {/* <!-- end container --> */}
-      </Fragment>
+      </div>
     )
   }
 }
