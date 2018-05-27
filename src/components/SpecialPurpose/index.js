5b725708151f4d08d8216e7764d949b71216a2ca
Alejandro Salazar <alejsala@cisco.com>
2018-05-27 16:23:43 -0700

    save the date 6

--- src/components/SpecialPurpose/index.js
+++ src/components/SpecialPurpose/index.js
@@ -3,7 +3,7 @@ import './styles.css'
 import mainImage from '../../assets/img/saveTheDate.jpg'
 import shrub from '../../assets/img/shrub.png'
 import shrub1 from '../../assets/img/shrub1.png'
-import plants from '../../assets/img/plants_web.png'
+import greenLeaf from '../../assets/img/greenLeaf.png'
 
 class SpecialPurpose extends Component {
   render() {
@@ -11,7 +11,7 @@ class SpecialPurpose extends Component {
       <div className="card">
         <img className="cover" src={mainImage} alt="Janelle and I" />
         <img className="shrub" src={shrub} alt="shrub" />
-        <img className="shrub1" src={shrub1} alt="shrub" />
+        <img className="shrub1" src={greenLeaf} alt="shrub" />
         <div className="text">
           <div className="title card-text">SAVE THE DATE</div>
           <div className="subtitle card-text">
