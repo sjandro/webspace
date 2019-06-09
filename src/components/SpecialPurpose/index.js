a43f70837b2bf593faeee74fe8b5dfee2fe59282
Alejandro Salazar <asalazar@C02Y73UWJGH6.local>
2019-06-08 23:52:08 -0700

    css

--- src/components/SpecialPurpose/index.js
+++ src/components/SpecialPurpose/index.js
@@ -11,7 +11,22 @@ import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
 import Card from 'react-bootstrap/Card'
 
+const CAVU = '27.9585751,-82.4621767'
+const ALEJANDRO = '28.2207775,-82.3119133'
+const JANELLE = '28.1845467,-82.3441757'
 class SpecialPurpose extends Component {
+  mapsSelector = (coord) => {
+    if (
+      /* if we're on iOS, open in Apple Maps */
+      navigator.platform.indexOf('iPhone') != -1 ||
+      navigator.platform.indexOf('iPad') != -1 ||
+      navigator.platform.indexOf('iPod') != -1
+    )
+      window.open(`maps://maps.google.com/maps?daddr=${coord}&amp;ll=`)
+    /* else use Google */ else
+      window.open(`https://maps.google.com/maps?daddr=${coord}&amp;ll=`)
+  }
+
   render() {
     return (
       <Card>
@@ -19,10 +34,13 @@ class SpecialPurpose extends Component {
         <Card.Body>
           <Tabs defaultActiveKey="itinerary" id="uncontrolled-tab-example">
             <Tab eventKey="itinerary" title="Itinerary">
-              <Itinerary />
+              <Itinerary clickHandler={this.mapsSelector} />
             </Tab>
-            <Tab eventKey="menu" title="Menu">
-              <Menu />
+            <Tab eventKey="food" title="Food">
+              <Food />
+            </Tab>
+            <Tab eventKey="drinks" title="Drinks">
+              <Drinks />
             </Tab>
             <Tab eventKey="save-the-date" title="Invite">
               <SaveTheDate />
@@ -34,34 +52,42 @@ class SpecialPurpose extends Component {
   }
 }
 
-const Itinerary = () => {
+const Itinerary = ({ clickHandler }) => {
   return (
     <div className="itinerary">
       <Card.Title className="header">Alejandro + Boys' Party</Card.Title>
       <Card.Text className="sub-header">Thursday, 6/20 @ 4:00PM</Card.Text>
-      <Card.Subtitle className="mb-2 text-muted">4403 Yans Ct.</Card.Subtitle>
-      <Card.Subtitle className="mb-2 text-muted">
-        Wesley Chapel, FL, 33543
-      </Card.Subtitle>
+      <div className="clickable" onClick={() => clickHandler(ALEJANDRO)}>
+        <Card.Subtitle className="mb-2 text-muted ">
+          4403 Yans Ct.
+        </Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted ">
+          Wesley Chapel, FL, 33543
+        </Card.Subtitle>
+      </div>
       <br />
       <Card.Title className="header">Janelle + Girls' Party</Card.Title>
       <Card.Text className="sub-header">Thursday, 6/20 @ 4:00PM</Card.Text>
-      <Card.Subtitle className="mb-2 text-muted">
-        1912 Twisting Ln.
-      </Card.Subtitle>
-      <Card.Subtitle className="mb-2 text-muted">
-        Wesley Chapel, FL, 33543
-      </Card.Subtitle>
+      <div className="clickable" onClick={() => clickHandler(JANELLE)}>
+        <Card.Subtitle className="mb-2 text-muted ">
+          1912 Twisting Ln.
+        </Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted ">
+          Wesley Chapel, FL, 33543
+        </Card.Subtitle>
+      </div>
       <br />
-      <Card.Title className="header">Wedding Ceremony + Reception</Card.Title>
-      <Card.Text className="sub-header">Saturday, 6/22 @ 6:30PM</Card.Text>
-      <Card.Subtitle className="mb-2 text-muted">CAVU Tampa</Card.Subtitle>
-      <Card.Subtitle className="mb-2 text-muted">
-        1601 N. Franklin St.
-      </Card.Subtitle>
-      <Card.Subtitle className="mb-2 text-muted">
-        Tampa Florida, 33602
-      </Card.Subtitle>
+      <Card.Title className="header ">Wedding Ceremony + Reception</Card.Title>
+      <Card.Text className="sub-header ">Saturday, 6/22 @ 6:30PM</Card.Text>
+      <div className="clickable" onClick={() => clickHandler(CAVU)}>
+        <Card.Subtitle className="mb-2 text-muted">CAVU Tampa</Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted">
+          1601 N. Franklin St.
+        </Card.Subtitle>
+        <Card.Subtitle className="mb-2 text-muted">
+          Tampa Florida, 33602
+        </Card.Subtitle>
+      </div>
 
       <Card.Text className="sub-header">6:30PM</Card.Text>
       <Card.Subtitle className="mb-2 text-muted">
@@ -78,7 +104,7 @@ const Itinerary = () => {
 
       <Card.Text className="sub-header">~7:45PM</Card.Text>
       <Card.Subtitle className="mb-2 text-muted">
-        Dinner Served (See “Menu”)
+        Dinner Served (See Food/Drinks)
       </Card.Subtitle>
 
       <Card.Text className="sub-header">~8:00PM</Card.Text>
@@ -88,7 +114,7 @@ const Itinerary = () => {
 
       <Card.Text className="sub-header">~8:30PM</Card.Text>
       <Card.Subtitle className="mb-2 text-muted">
-        Coffee and Dessert Served (See “Menu”)
+        Coffee and Dessert Served (See Food/Drinks)
       </Card.Subtitle>
 
       <Card.Text className="sub-header">~8:45PM</Card.Text>
@@ -110,7 +136,7 @@ const Itinerary = () => {
   )
 }
 
-const Menu = () => {
+const Food = () => {
   return (
     <div className="menu">
       <Card.Title className="header">Tampa Cubano Mains</Card.Title>
@@ -133,7 +159,9 @@ const Menu = () => {
 
       <Card.Title className="header">Fixings</Card.Title>
       <Card.Subtitle className="mb-2 text-muted">Black Beans</Card.Subtitle>
-      <Card.Subtitle className="mb-2 text-muted">Tostones</Card.Subtitle>
+      <Card.Subtitle className="mb-2 text-muted">
+        Tostones/Patacones
+      </Card.Subtitle>
       <Card.Subtitle className="mb-2 text-muted">Jasmine Rice</Card.Subtitle>
       <br />
 
@@ -141,6 +169,49 @@ const Menu = () => {
       <Card.Subtitle className="mb-2 text-muted">Garlic Mojo</Card.Subtitle>
       <Card.Subtitle className="mb-2 text-muted">Sofrito</Card.Subtitle>
       <Card.Subtitle className="mb-2 text-muted">Mojo Criollo</Card.Subtitle>
+      <br />
+
+      <Card.Title className="header">Desserts</Card.Title>
+      <Card.Subtitle className="mb-2 text-muted">
+        Tres Leches Shooters
+      </Card.Subtitle>
+      <Card.Subtitle className="mb-2 text-muted">
+        Double Chocolate Brownie Bites
+      </Card.Subtitle>
+    </div>
+  )
+}
+
+const Drinks = () => {
+  return (
+    <div className="drinks">
+      <Card.Title className="header">Alcohol-Free</Card.Title>
+      <Card.Text className="sub-header">Refreshments</Card.Text>
+      <Card.Subtitle className="mb-2 text-muted">
+        Water, Coffee, etc...
+      </Card.Subtitle>
+      <br />
+      <Card.Title className="header">Alcohol</Card.Title>
+      <Card.Text className="sub-header">Beer</Card.Text>
+      <Card.Subtitle className="mb-2 text-muted">
+        Domestic (2): Corona, Budweiser
+      </Card.Subtitle>
+      <Card.Subtitle className="mb-2 text-muted">
+        Craft (2): Guayabera Citra Pale Ale, New Belgium Fat Tire
+      </Card.Subtitle>
+      <Card.Text className="sub-header">Red Wine</Card.Text>
+      <Card.Subtitle className="mb-2 text-muted">
+        La Crema Pinot Noir (Monterey, CA); Hess Cabernet (Napa Allomi Vineyard,
+        CA);{' '}
+      </Card.Subtitle>
+      <Card.Text className="sub-header">Roséy/Sparkling Wine</Card.Text>
+      <Card.Subtitle className="mb-2 text-muted">
+        La Crema Rosé (CA); La Vostra Prosecco (Italy){' '}
+      </Card.Subtitle>
+      <Card.Text className="sub-header">Speciality Cocktail</Card.Text>
+      <Card.Subtitle className="mb-2 text-muted">Margarita</Card.Subtitle>
+      <Card.Text className="sub-header">Liquors/Spirits</Card.Text>
+      <Card.Subtitle className="mb-2 text-muted">... </Card.Subtitle>
     </div>
   )
 }
