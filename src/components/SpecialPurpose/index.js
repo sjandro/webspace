2087084a3b94c4fb6093eaf99ccdb4e5044537f7
Alejandro Salazar <asalazar@C02Y73UWJGH6.local>
2019-06-08 23:54:00 -0700

    fsd

--- src/components/SpecialPurpose/index.js
+++ src/components/SpecialPurpose/index.js
@@ -16,15 +16,15 @@ const ALEJANDRO = '28.2207775,-82.3119133'
 const JANELLE = '28.1845467,-82.3441757'
 class SpecialPurpose extends Component {
   mapsSelector = (coord) => {
-    if (
-      /* if we're on iOS, open in Apple Maps */
-      navigator.platform.indexOf('iPhone') != -1 ||
-      navigator.platform.indexOf('iPad') != -1 ||
-      navigator.platform.indexOf('iPod') != -1
-    )
-      window.open(`maps://maps.google.com/maps?daddr=${coord}&amp;ll=`)
-    /* else use Google */ else
-      window.open(`https://maps.google.com/maps?daddr=${coord}&amp;ll=`)
+    // if (
+    //   /* if we're on iOS, open in Apple Maps */
+    //   navigator.platform.indexOf('iPhone') != -1 ||
+    //   navigator.platform.indexOf('iPad') != -1 ||
+    //   navigator.platform.indexOf('iPod') != -1
+    // )
+    //   window.open(`maps://maps.google.com/maps?daddr=${coord}&amp;ll=`)
+    // /* else use Google */ else
+    window.open(`https://maps.google.com/maps?daddr=${coord}&amp;ll=`)
   }
 
   render() {
